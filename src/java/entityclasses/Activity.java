303f6dda9e53d75de5fbf3f78eeeac73cbd380ef
Luke Mazzu <mluke94@vt.edu>
2017-04-18 12:49:22 -0400

    Entity classes created and glassfish server added

---
+++ src/java/entityclasses/Activity.java
@@ -0,0 +1,108 @@
+/*
+ * Created by Luke Mazzu on 2017.04.18  * 
+ * Copyright © 2017 Luke Mazzu. All rights reserved. * 
+ */
+package entityclasses;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Luke
+ */
+@Entity
+@Table(name = "Activity")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Activity.findAll", query = "SELECT a FROM Activity a")
+    , @NamedQuery(name = "Activity.findById", query = "SELECT a FROM Activity a WHERE a.id = :id")
+    , @NamedQuery(name = "Activity.findByAction", query = "SELECT a FROM Activity a WHERE a.action = :action")})
+public class Activity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Basic(optional = false)
+    @Column(name = "action")
+    private String action;
+    @JoinColumn(name = "note_id", referencedColumnName = "id")
+    @ManyToOne
+    private Notes noteId;
+
+    public Activity() {
+    }
+
+    public Activity(Integer id) {
+        this.id = id;
+    }
+
+    public Activity(Integer id, String action) {
+        this.id = id;
+        this.action = action;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public Notes getNoteId() {
+        return noteId;
+    }
+
+    public void setNoteId(Notes noteId) {
+        this.noteId = noteId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Activity)) {
+            return false;
+        }
+        Activity other = (Activity) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entityclasses.Activity[ id=" + id + " ]";
+    }
+    
+}
---
+++ src/java/entityclasses/UserFile.java
@@ -0,0 +1,108 @@
+/*
+ * Created by Luke Mazzu on 2017.04.18  * 
+ * Copyright © 2017 Luke Mazzu. All rights reserved. * 
+ */
+package entityclasses;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Luke
+ */
+@Entity
+@Table(name = "UserFile")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "UserFile.findAll", query = "SELECT u FROM UserFile u")
+    , @NamedQuery(name = "UserFile.findById", query = "SELECT u FROM UserFile u WHERE u.id = :id")
+    , @NamedQuery(name = "UserFile.findByFilename", query = "SELECT u FROM UserFile u WHERE u.filename = :filename")})
+public class UserFile implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Basic(optional = false)
+    @Column(name = "filename")
+    private String filename;
+    @JoinColumn(name = "note_id", referencedColumnName = "id")
+    @ManyToOne
+    private Notes noteId;
+
+    public UserFile() {
+    }
+
+    public UserFile(Integer id) {
+        this.id = id;
+    }
+
+    public UserFile(Integer id, String filename) {
+        this.id = id;
+        this.filename = filename;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    public Notes getNoteId() {
+        return noteId;
+    }
+
+    public void setNoteId(Notes noteId) {
+        this.noteId = noteId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof UserFile)) {
+            return false;
+        }
+        UserFile other = (UserFile) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entityclasses.UserFile[ id=" + id + " ]";
+    }
+    
+}
